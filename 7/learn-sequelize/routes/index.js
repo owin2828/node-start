c910d16d4795e2f846df3970c884a0c316bf7e21
owin <owin2828@gmail.com>
2020-07-15 07:50:15 +0900

    [ADD] 7

--- 7/learn-sequelize/routes/index.js
+++ 7/learn-sequelize/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var User = require('../models').User;
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  /*
+   * 다음의 코드는 프로미스를 사용한 코드
+   * 그 아래의 async/await를 사용한 코드와 동일한 기능을 제공한다.
+  User.findAll()
+    .then((users) => {
+      res.render('sequelize', {users});
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+  */
+  try{
+    const users = await User.findAll();
+    res.render('sequelize', {users});
+  }
+  catch(error){
+    console.log(error);
+    next(error);
+  }
 });
 
 module.exports = router;
