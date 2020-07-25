02fae905e1c11d4b1407e657b84f38f3f9461a4d
owin <owin2828@gmail.com>
2020-07-25 18:55:30 +0900

    [ADD] 9

--- 9/nodebird/app.js
+++ 9/nodebird/app.js
@@ -4,46 +4,62 @@ const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+require('dotenv').config();
 
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
+sequelize.sync();
+passportConfig(passport);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('port', process.env.PORT || 8081);
+app.set('port', process.env.PORT || 8001);
 
-app.unsubscribe(morgan('dev'));
-app.unsubscribe(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use(cookieParser('nodebirdsecret'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'nodebirdsecret',
-    cookie: {
-        httpOnly: true;
-        secure: false,
-    },
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
-app.use(falsh());
+app.use(flash());
+app.use(passport.initialize());
+
+/* 
+ * passport.session() 미들웨어는 req.session 객체에 passport 정보를 저장
+ * req.session 객체는 express에서 생성하는 것이므로,
+ * passport 미들웨어는 express-session 미들웨어보다 뒤에 연결해야 함
+ */
+app.use(passport.session());
 
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
 
-app.use((req,res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.use((err, req, res, next => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-}));
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중');
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
---
+++ 9/nodebird/passport/index.js
@@ -0,0 +1,50 @@
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const { User } = require('../models');
+
+/*
+ * Passport 모듈 설정 파일
+ *
+ * 로그인한 유저의 id를 세션에 저장하는 부분과
+ * 저장한 유저의 id를 기반으로 DB에서 정보를 조회하여 req.user에 저장하는 부분으로 구성
+ * 전체적인 흐름은 다음과 같다
+ * 
+ * ## 1. 유저가 로그인 하기까지 
+ * 1-1. 로그인 요청이 들어옴
+ * 1-2. passport.authenticate 매서드 호출
+ * 1-3. 로그인 전략 수행
+ * 1-4. 로그인 성공 시, 사용자 정보 객체와 함께 req.login 호출
+ * 1-5. req.login 매서드가 passport.serializeUser 호출
+ * 1-6. req.session에 사용자 아이디만 저장
+ * 1-7. 로그인 완료
+ * 
+ * ## 2. 로그인 이후의 과정
+ * 2-1. 모든 요청에 passport.session() 미들웨어가 passport.deserializeUser 매서드 호출
+ * 2-2. req.session에 저장된 아이디로 DB에서 사용자 조회
+ * 2-3. 조회된 사용자 정보를 req.user에 저장
+ * 2-4. 라우터에서 req.user 객체 사용 가능
+ */
+module.exports = (passport) => {
+    /*
+     * 사용자 정보 객체를 세션에 아이디로 저장
+     * 매개변수 user에 로그인 사용자 정보가 담기고,
+     * done에 두번째 인자로 id를 넘김 (첫 번째는 err 처리, 두 번째가 중요)
+     */
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    /*
+     * 세션에 저장한 아이디를 통해 사용자 정보 객체를 불러옴
+     * 위에서 세션에 저장한 id를 받아 DB에서 사용자 정보를 조회
+     * 조회한 정보는 req.user에 저장하므로, 앞으로 사용자 정보는 req.user를 통해 조회
+     */
+    passport.deserializeUser((id, done) => {
+        User.findOne({ where: { id } })
+            .then(user => done(null, user /* <- req.user에 저장 */)) 
+            .catch(err => done(err));
+    });
+
+    local(passport);
+    kakao(passport);
+};
